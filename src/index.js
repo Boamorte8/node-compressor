f2950fd31047f74dc3f011dbf160a68f359c7149
Esteban Salazar Boamorte <salazar.z.esteban@gmail.com>
2022-08-28 21:07:26 -0500

    feat(steps): write steps to build app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import { printHello } from '#Lib/hello.js';
-
-printHello();
+// 1. Get the path
+// 2. Check if document exists
+// 3. Read the document
+// 4. Compress the document
+// 5. Write the document
+// 6. Add progress bar
+// 7. Add pause control
+// 8. Add resume control
